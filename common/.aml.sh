be9b888100ad3019c51df13a2a6b6882097cb6e4
acervenky <acervenky@gmail.com>
2018-12-05 17:14:24 +0530

    Update .aml.sh

--- common/.aml.sh
+++ common/.aml.sh
@@ -16,16 +16,16 @@ patch_mixer_toplevel -c "SLIM RX0 MUX" "AIF1_PB"
 patch_mixer_toplevel -c "RX INT0_1 MIX1 INP0" "RX0"
 sed -i "/ *<path name=\"deep-buffer-playback quat_i2s\">/a\      <ctl name=\"SLIMBUS_0_RX Audio Mixer MultiMedia1\" value=\"1\" \/>\n\
       <ctl name=\"RX0 HPF cut off\" value=\"MIN_3DB_150Hz\" \/>\n\
-      <ctl name=\"RX0 Digital Volume\" value=\"88\" \/>\n\
-      <ctl name=\"RX7 Digital Volume\" value=\"88\" \/>\n\
+      <ctl name=\"RX0 Digital Volume\" value=\"84\" \/>\n\
+      <ctl name=\"RX7 Digital Volume\" value=\"84\" \/>\n\
       <ctl name=\"SLIM_0_RX SampleRate\" value=\"KHZ_96\" \/>" $MODPATH/system/vendor/etc/mixer_paths_tavil.xml
 sed -i "/ *<path name=\"low-latency-playback quat_i2s\">/a\      <ctl name=\"SLIMBUS_0_RX Audio Mixer MultiMedia5\" value=\"1\" \/>\n\
       <ctl name=\"RX0 HPF cut off\" value=\"MIN_3DB_150Hz\" \/>\n\
-      <ctl name=\"RX0 Digital Volume\" value=\"88\" \/>\n\
-      <ctl name=\"RX7 Digital Volume\" value=\"88\" \/>\n\
+      <ctl name=\"RX0 Digital Volume\" value=\"84\" \/>\n\
+      <ctl name=\"RX7 Digital Volume\" value=\"84\" \/>\n\
       <ctl name=\"SLIM_0_RX SampleRate\" value=\"KHZ_96\" \/>" $MODPATH/system/vendor/etc/mixer_paths_tavil.xml
 sed -i "/ *<path name=\"compress-offload-playback quat_i2s\">/a\      <ctl name=\"SLIMBUS_0_RX Audio Mixer MultiMedia4\" value=\"1\" \/>\n\
       <ctl name=\"RX0 HPF cut off\" value=\"MIN_3DB_150Hz\" \/>\n\
-      <ctl name=\"RX0 Digital Volume\" value=\"88\" \/>\n\
-      <ctl name=\"RX7 Digital Volume\" value=\"88\" \/>\n\
+      <ctl name=\"RX0 Digital Volume\" value=\"84\" \/>\n\
+      <ctl name=\"RX7 Digital Volume\" value=\"84\" \/>\n\
       <ctl name=\"SLIM_0_RX SampleRate\" value=\"KHZ_96\" \/>" $MODPATH/system/vendor/etc/mixer_paths_tavil.xml
